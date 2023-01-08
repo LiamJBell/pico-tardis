8aa52dfa5082bea388dd4160a30acf1fd320badd
Liam J Bell <Liam J Bell>
2023-01-08 01:47:01 +0000

    wrote web server and implemented takeoff command

--- code.py
+++ code.py
@@ -3,22 +3,28 @@ import board, os, pwmio, asyncio
 import wifi, socketpool
 from adafruit_httpserver.server import HTTPServer
 from adafruit_httpserver.response import HTTPResponse
+from adafruit_httpserver.request import HTTPRequest
+from adafruit_httpserver.mime_type import MIMEType
 from adafruit_itertools import chain
 from audiomp3 import MP3Decoder
+import microcontroller
 
 audio = audiobusio.I2SOut(board.GP10, board.GP11, board.GP9)
 pwm = pwmio.PWMOut(board.GP17)
+pool = socketpool.SocketPool(wifi.radio)
+server = HTTPServer(pool)
 
-async def selectSound():
+async def selectSound(command):
     # do sound selection stuff
-    return
+    if(command=="takeoff"):
+        return "sounds/defaultFlightLaunch.mp3"
+    return False
 
-
-async def playSound():
+async def playSound(sound):
     if audio.playing:
         return False
     else:
-        mp3 = open("sounds/defaultFlightLaunch.mp3", "rb")
+        mp3 = open(sound, "rb")
         decoder = MP3Decoder(mp3)
         audio.play(decoder)
         asyncio.run(light())
@@ -33,23 +39,56 @@ async def light():
             pwm.duty_cycle = i*i
             await asyncio.sleep(0.004)
 
-async def connectToWiFi():    
-    try:
-        from secrets import secrets
-    except ImportError:
-        print("WiFi secrets are kept in secrets.py")
-        raise
-    wifi.radio.hostname="TARDIS"
-    wifi.radio.connect(secrets["ssid"], secrets["password"])
-    print("Connected to WiFi %s on IP:" % secrets["ssid"], wifi.radio.ipv4_address)
+@server.route("/command")
+def handleTardisCommand(request):
+    command = request.query_params.get("a")
+    response = HTTPResponse(request, content_type=MIMEType.TYPE_TXT)
+    sound = asyncio.run(selectSound(command))
+    if sound:
+        response.send("OK")
+        asyncio.run(playSound(sound))
+    else:
+        response.send("Invalid parameter")
 
+
+
+async def connectToWiFi():  
+    # lock = asyncio.Lock()
+    # with await lock.acquire():
+        wifi.radio.hostname="TARDIS"
+        wifi.radio.connect(os.getenv("CIRCUITPY_WIFI_SSID"), os.getenv("CIRCUITPY_WIFI_PASSWORD"))
+        print("Connected to WiFi %s on IP:" % os.getenv("CIRCUITPY_WIFI_SSID"), wifi.radio.ipv4_address)
+    # lock.release()
+
+
+async def startHTTPServer():
+    global server    
+    try:
+        server.start(str(wifi.radio.ipv4_address))
+        print("listening on:%s" % wifi.radio.ipv4_address)
+        return True
+    except OSError:
+        asyncio.sleep(5)
+        print("restarting. . .")
+        microcontroller.reset()
+    
+async def loop():
+    while True:
+        server.poll()
 
 async def main():
+    global server
     wifi = asyncio.create_task(connectToWiFi())
-    
-    await asyncio.gather(wifi)
+    startServer = asyncio.create_task(startHTTPServer())
+    asyncio.gather(wifi, startServer)
 
+    asyncio.run(loop())
 
 
 if __name__ == "__main__":
     asyncio.run(main())
+
+
+
+
+
